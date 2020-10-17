1918263ea3876089e789c8d2636f99065a1df2d8
Dan Whittaker <Dan.Whittaker@causeway.com>
2020-10-17 18:55:07 +0100

    Moved to separate folder & created custom server. This is BEFORE custom server added

---
+++ server-azure/Functions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace server_azure
+{
+    public static class Functions
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "game")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("messages")]
+        public static Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] dynamic message,
+            [SignalR(HubName = "game")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            string roomName = message.room;
+
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    GroupName = roomName,
+                    Target = "newMessage",
+                    Arguments = new[] { message }
+                });
+        }
+    }
+}
